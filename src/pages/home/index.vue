d1823bb2cadf651cf8bad2ada16b296cec3da978
yankun <15635699721@163.com>
2019-06-28 20:16:13 +0800

    下拉刷新，回到顶部完成

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,88 +1,92 @@
 <template>
   <div>
+    <!-- 搜索栏 -->
     <div class="search">
       <div class="search-item">
         <icon type="search"/>
         <span>搜索</span>
       </div>
     </div>
+    <!-- 轮播图 -->
     <swiper indicator-dots="true">
       <swiper-item v-for="(item,index) in swiperData" :key="index">
         <img class="swiperImg" :src="item.image_src">
       </swiper-item>
     </swiper>
+    <!-- 菜单栏 -->
     <div class="menu">
       <div class="menu-item" v-for="(item,index) in menuData" :key="index">
         <img :src="item.image_src">
       </div>
     </div>
+    <!-- 商品列表 -->
+    <div class="floor" v-for="(item,index) in floorData" :key="index">
+      <div class="floor-title">
+        <img :src="item.floor_title.image_src">
+      </div>
+      <div class="floor-content">
+        <div class="left">
+          <img :src="item.leftSrc">
+        </div>
+        <div class="right">
+          <img :src="e.image_src" v-for="(e,i) in item.rightSrc" :key="i">
+        </div>
+      </div>
+    </div>
+    <div class="top" v-show="flag" @click="toTop">
+      ︿
+      <p>顶部</p>
+    </div>
   </div>
 </template>
 <script>
-import request from "../../utils/request"
+import request from "../../utils/request";
+import "./main.less";
 export default {
   data() {
     return {
-      swiperData:[],
-      menuData:[]
+      swiperData: [],
+      menuData: [],
+      floorData: [],
+      flag: false
     };
   },
+  computed: {
+    floornewData() {
+      return this.floorData.map(item => {
+        item.leftSrc = item.product_list[0].image_src;
+        item.rightSrc = item.product_list.filter((e, i) => {
+          return i > 0;
+        });
+        return item;
+      });
+    }
+  },
   methods: {
-    async init(){
-      let swiperRes = await request("/home/swiperdata")
-      this.swiperData=swiperRes.data.message
-      let menuRes = await request("/home/catitems")
-      this.menuData=menuRes.data.message
+    toTop() {
+      wx.pageScrollTo({
+        scrollTop: 0
+      });
+    },
+    async init() {
+      let swiperRes = await request("/home/swiperdata");
+      this.swiperData = swiperRes.data.message;
+      let menuRes = await request("/home/catitems");
+      this.menuData = menuRes.data.message;
+      let floorRes = await request("/home/floordata");
+      this.floorData = floorRes.data.message;
     }
   },
   created() {
-    this.init()
-    // request("/home/swiperdata").then(res=>{
-    //   this.swiperData=res.data.message
-    // })
-    // mpvue.request({
-    //   url:"https://www.zhengzhicheng.cn/api/public/v1/home/swiperdata",
-    //   method:"GET",
-    //   success:(res)=>{
-    //     this.swiperData=res.data.message
-    //   }
-    // })
-    // mpvue.request({
-    //   url:"https://www.zhengzhicheng.cn/api/public/v1/home/catitems",
-    //   method:"GET",
-    //   success:(res)=>{
-    //     this.menuData=res.data.message
-    //   }
-    // })
+    this.init();
   },
+  onPageScroll(e) {
+    this.flag = e.scrollTop > 100;
+  },
+  onPullDownRefresh() {
+    this.init();
+  }
 };
 </script>
 <style scoped>
-.search {
-  background-color: #EB4450;
-  padding: 10px;
-}
-.search-item {
-  background-color: #fff;
-  text-align: center
-}
-
-.search-item icon {
-  vertical-align: middle
-}
-.swiperImg {
-  width: 750rpx;
-}
-.menu {
-  margin-top: 10rpx;
-  display: flex;
-}
-.menu .menu-item {
-  flex: 1;
-}
-.menu .menu-item image {
-  width: 180rpx;
-  height: 170rpx;
-}
-
 </style>
