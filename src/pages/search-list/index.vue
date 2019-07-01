638a33f15fcdc118c8187c9d077b7327df419f7c
yankun <15635699721@163.com>
2019-07-01 08:59:19 +0800

    搜索功能完成

---
+++ src/pages/search-list/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>123</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    onLoad(e){
+        console.log(e)
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import "./main.less";
+</style>
--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -1,45 +1,71 @@
 <template>
-    <div>
+  <div>
     <!-- 搜索条 -->
     <div class="search">
       <div class="search-content">
         <div class="search-input">
-          <icon type='search' size='16'/>
-          <input @input='handleInput' v-model='keyword' placeholder="请输入关键字" />
+          <icon type="search" size="16"/>
+          <input @input="handleInput" @confirm="confirm" v-model="keyword" placeholder="请输入关键字">
         </div>
-        <button class="cancel" v-if='keyword' @click="cancel()">取消</button>
-        <div class="search-modal" v-if='keyword'>
-          <div :key='item.goods_id' v-for='item in resultList' class="search-item">{{item.goods_name}}</div>
+        <button class="cancel" v-if="keyword" @click="cancel()">取消</button>
+        <div class="search-modal" v-if="keyword">
+          <div
+            :key="item.goods_id"
+            v-for="item in resultList"
+            class="search-item"
+          >{{item.goods_name}}</div>
         </div>
       </div>
     </div>
-    <div class="history"></div>
-    <div class="history-list"></div>
+    <div class="history">
+      <h4>搜索历史</h4>
+      <icon type="clear" size="16" @click="clearHistory"></icon>
+    </div>
+    <div class="history-list">
+      <div :key="index" v-for="(item, index) in historyKey" class="history-item">
+        <navigator :url="'/pages/search-list/main?key=' + item">{{item}}</navigator>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import request from '../../utils/request.js'
-
+import request from "../../utils/request.js";
 export default {
-  data () {
+  data() {
     return {
-      keyword: '',
+      keyword: "",
       resultList: [],
-      timer:null
-    }
+      timer: null,
+      historyKey: mpvue.getStorageSync("") || []
+    };
   },
   methods: {
-    cancel(){
-      this.keyword=''
+    clearHistory() {
+      mpvue.clearStorageSync("history");
+      this.historyKey = [];
+    },
+    confirm() {
+      if (!this.keyword) {
+        return;
+      }
+      this.historyKey.unshift(this.keyword);
+      this.historyKey = [...new Set(this.historyKey)];
+      mpvue.setStorageSync("history", this.historyKey);
+      mpvue.navigateTo({
+        url:"/pages/search-list/main?key=" + this.keyword
+      })
+    },
+    cancel() {
+      this.keyword = "";
     },
     // 防抖
-    handleInput(){
-      clearTimeout(this.timer)
-      this.timer=setTimeout(async ()=>{
-        let res = await request('/goods/qsearch', {query: this.keyword})
-        this.resultList = res.data.message
-      },1000)
+    handleInput() {
+      clearTimeout(this.timer);
+      this.timer = setTimeout(async () => {
+        let res = await request("/goods/qsearch", { query: this.keyword });
+        this.resultList = res.data.message;
+      }, 1000);
     }
     // async handleInput() {
     //   if(this.isLoading){
@@ -51,10 +77,10 @@ export default {
     //   this.isLoading = false
     // }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
-  @import "./main.less";
+@import "./main.less";
 </style>
 
